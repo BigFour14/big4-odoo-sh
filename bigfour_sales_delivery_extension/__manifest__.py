0f857e309b6821f41e8b988f590790e8afb68145
BigFour14 <Info@big4eg.com>
2025-07-09 01:20:42 +0300

    Create __manifest__.py

---
+++ bigfour_sales_delivery_extension/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'BigFour Sales Delivery Extension',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Custom delivery workflow enhancements for BigFour',
+    'depends': ['sale', 'stock'],
+    'data': [
+        # اضف ملفات XML لو موجودة
+    ],
+    'installable': True,
+    'application': False,
+}
